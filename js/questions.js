cf562cbbf1494f552478be75d2725d4812e75267
Hyperloll <90119351+Hyperloll@users.noreply.github.com>
2021-09-30 19:51:26 -0400

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the lcm of 10",
+    answer: "10",
+    options: [
+      "10",
+      "5",
+      "100",
+      "78"
+    ]
+  },
+    {
+    numb: 2,
+    question: "what is the lcm of 7 and 8",
+    answer: "56",
+    options: [
+      "60",
+      "200",
+      "56",
+      "100"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the LCM of 16 and 24",
+    answer: "48",
+    options: [
+      "1",
+      "48",
+      "42",
+      "61"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The product of two co-prime numbers is always equal to their:",
+    answer: "LCM",
+    options: [
+      "sum",
+      "difference",
+      "LCM",
+      "ratio"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Least common multiple of 5, 10 and 15 is",
+    answer: "30",
+    options: [
+      "1",
+      "1000",
+      "29",
+      "30"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
